08c5ec341340c91efe8b9289e58c16c23df68dcb
YoungSandw1ch <sersus1991@gmail.com>
2022-01-14 13:33:22 +0000

    Deploying to gh-pages from @ YoungSandw1ch/team-project-icecream@53fa75f6580c2c60021fc3e37cb3bcc8e02cdea9 🚀

--- src.ac09a9c7.js
+++ src.ac09a9c7.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-icecream/src.206d6079.js.map
+//# sourceMappingURL=/team-project-icecream/src.ac09a9c7.js.map
